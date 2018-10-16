ee5864e87e4fdd5e7699ebb2bd5d022ea9daf100
SeakyLuo <haitianluo@umail.ucsb.edu>
2018-10-16 00:28:42 -0700

    Improvements

--- Python Files/Simplifier/simplifier.py
+++ Python Files/Simplifier/simplifier.py
@@ -2,233 +2,235 @@ from tkinter import *
 from tkinter import messagebox
 from eztk import *
 import ez
+from win32 import win32clipboard
 
-root=Tk()
-root['bg']='MintCream'
-root.title('Simplifier')
-btxt='button.txt'
-try: open(btxt, 'x').close()
-except FileExistsError: pass
+class Simplifier(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+        self.title('Simplifier')
+        self.btxt = 'button.txt'
+        try: open(self.btxt, 'x').close()
+        except FileExistsError: pass
+        self.wrappers = ez.fread(self.btxt, True)
+        if self.wrappers == '': self.wrappers = {'newline': 0, 'brackets': 0, 'numbers': 0, 'smartnl': 1,'space': 1, 'autocopy': 1, 'translate': 0, 'lines': 1, 'chars': 0, 'fontsize': 10}
+        self.history = ['']
+        self.pointer = 0
+        self.ctrlCounter = self.yzCounter = 0
+        self.brackets = {'[': ']', '(': ')', '{': '}', '（': '）',\
+        '【': '】', '<': '>', '《': '》', '『': '』', '「': '」'}
+        self.rbrackets = { self.brackets[key]: key for key in self.brackets }
+        
+        self.buttonbar = Frame(self)
+        self.input = Frame(self)
+        self.widgetbar = Frame(self)
+        self.output = Frame(self, width = 600, height = 800)
+        self.output.grid_propagate(False)
+        frames = [self.buttonbar, self.input, self.widgetbar, self.output]
+        self.t1 = Text(self.input)
+        self.t1.bind('<KeyRelease>', self.monitor)
+        self.w0 = Label(self.widgetbar, text = 'In↑Out↓')
+        self.w1 = Button(self.widgetbar, command = self.lineCount)
+        self.w2 = Button(self.widgetbar, command = self.charCount)
+        self.w3 = Button(self.widgetbar, text = 'Clear',command = self.clear)
+        self.w4 = Button(self.widgetbar, text = 'AutoCopy', command = self.autocopy)
+        self.w5 = Button(self.widgetbar, text = 'Paste', command = self.paste)
+        self.w6 = Button(self.widgetbar, text = 'Undo', command = self.ButtonUndo)
+        self.t1.bind('<Control-z>', lambda event: self.undo())
+        self.w7 = Button(self.widgetbar,text = 'Redo', command = self.ButtonRedo)
+        self.t1.bind('<Control-y>', lambda event: self.redo())
+        self.w8 = Scale(self.widgetbar, from_ = 5, to = 50, orient = HORIZONTAL, showvalue = 0, command = lambda event: self.fontSize())
+        self.b0 = Button(self.buttonbar, text = "\\n", command = lambda: self.remover('newline'))
+        self.b1 = Button(self.buttonbar, text = '([{}])', command = lambda: self.remover('brackets'))
+        self.b2 = Button(self.buttonbar, text = '1', command = lambda: self.remover('numbers'))
+        self.b3 = Button(self.buttonbar, text = 'smartnl', command = lambda: self.remover('smartnl'))
+        self.b4 = Button(self.buttonbar, text = 'space', command = lambda: self.remover('space'))
+        self.b5 = Button(self.buttonbar, text = 'translate', command = lambda: self.remover('translate'))
+        buttons = [self.b0, self.b1, self.b2, self.b3, self.b4, self.b5]
+        widgets = [self.w0, self.w1, self.w2, self.w3, self.w4, self.w5, self.w6, self.w7, self.w8]
+        self.wd = {'newline': self.b0, 'brackets': self.b1, 'numbers': self.b2, 'smartnl': self.b3, \
+        'autocopy': self.w4, 'space': self.b4, 'translate': self.b5}
+        columnspanLength = max(len(widgets),len(buttons))
+        for f in frames:
+            f['bg'] = 'MintCream'
+            f.pack(fill = BOTH, expand = 1)
+            f.rowconfigure(0, weight = 1)
+            f.columnconfigure(0, weight = 1)
+        for w in buttons:
+            w.configure(relief = FLAT,bg = 'SeaGreen1')
+            w.pack(side = LEFT, fill = Y, expand = 1)
+        self.t1.grid(columnspan = columnspanLength, sticky = NSEW)
+        for w in widgets:
+            w.configure(relief = FLAT, bg = 'thistle1')
+            w.pack(side = LEFT, fill = Y, expand = 1)
+        self.t2 = Text(self.output)
+        self.t2.grid(columnspan = columnspanLength, sticky = NSEW)
+        self.wrapper()
+        self.w8.set(self.wrappers['fontsize'])
+        self.fontSize()
 
-t1=Text(root)
-
-history = ['']
-pointer = 0
-ctrlCounter = yzCounter = 0
-wrappers = ez.fread(btxt, True)
-if wrappers == '':
-    wrappers = {'newline': 0, 'brackets': 0, 'numbers': 0, 'smartnl': 1,'space': 1, 'autocopy': 1, 'translate': 0, 'lines': 1, 'chars': 0}
-
-def monitor(event):
-    global history, pointer, ctrlCounter, yzCounter
-    t = gettxt(t1)
-    if not t or t == history[-1]:
-        return
-    if ctrlCounter and 'Control' in event.keysym:
-        ctrlCounter -= 1
-    elif yzCounter and event.keysym in ['y', 'z']:
-        yzCounter -= 1
-    else:
-        history = history[:pointer + 1] + [t]
-        pointer = len(history) - 1
-    wrapper()
-t1.bind('<KeyRelease>',monitor)
-
-w0 = Label(root,text = 'In↑Out↓')
-def countLines():
-    if wrappers['lines']: 
-        count = lambda t: t.count('\n')+(t[-1] != '\n') if t else 0
-        up = count(gettxt(t1))
-        down = count(gettxt(t2))
-        w1['text'] = f'Lines↑{up}↓{down}'
-    else:
-        w1['text'] = 'LineCount'
-def lineCount():
-    wrappers['lines'] = int(not wrappers['lines'])
-    countLines()
-root.bind('<KeyRelease>', lambda event: countLines())
-w1 = Button(root, command = lineCount)
-def countChars():
-    if wrappers['chars']:
-        up = len(gettxt(t1))
-        down = len(gettxt(t2))
-        w2['text'] = f'Chars↑{up}↓{down}'
-    else:
-        w2['text'] = 'CharCount'
-def charCount():
-    wrappers['chars'] = int(not wrappers['chars'])
-    countChars()    
-root.bind('<KeyRelease>', lambda event: countChars())
-w2 = Button(root, command = charCount)
-def clear():
-    deltxt(t1)
-    deltxt(t2)
-w3 = Button(root,text = 'Clear',command = clear)
-def autocopy():
-    switchButtonState(w4, 'autocopy')
-    ez.fwrite(btxt, wrappers)
-    ez.cpc(gettxt(t2))
-w4 = Button(root,text = 'AutoCopy', command = autocopy)
-def paste():
-    from win32 import win32clipboard
-    win32clipboard.OpenClipboard()
-    data = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
-    instxt(t1, data)
-    wrapper()
-w5 = Button(root,text = 'Paste', command = paste)
-def undo():
-    global history, pointer, ctrlCounter, yzCounter
-    if not pointer: return
-    pointer -= 1
-    deltxt(t1)
-    instxt(t1, history[pointer])
-    ctrlCounter = yzCounter = 1
-def ButtonUndo():
-    undo()
-    wrapper()
-w6 = Button(root,text = 'Undo', command = ButtonUndo)
-def KeyboardUndo(event):
-    undo()
-t1.bind('<Control-z>', KeyboardUndo)
-def redo():
-    global history, pointer, ctrlCounter, yzCounter
-    if pointer == len(history) - 1: return
-    pointer += 1
-    deltxt(t1)
-    instxt(t1, history[pointer])
-    ctrlCounter = yzCounter = 1
-def ButtonRedo():
-    redo()
-    wrapper()
-w7 = Button(root,text = 'Redo', command = ButtonRedo)
-def KeyboardURedo(event):
-    redo()
-t1.bind('<Control-y>', KeyboardURedo)
-
-def wrapper():
-    global wrappers, wd
-    string=gettxt(t1)
-    for func in wd:
-        if wrappers.get(func, 0):
-            if func == 'newline': string = newline_remover(string)
-            elif func == 'brackets': string = brackets_remover(string)
-            elif func == 'numbers': string = numbers_remover(string)
-            elif func == 'smartnl': string = smart_newline_remover(string)
-            wd[func]['relief'] = GROOVE
-            
-    if wrappers['space']: string = space_remover(string)
-    if wrappers['translate']: string = translate(string)
-    ez.fwrite(btxt, wrappers)
-    deltxt(t2)
-    instxt(t2, string)
-    countLines()
-    countChars()
-    if wrappers['autocopy']:
-        try:
-            ez.cpc(string)
-        except UnicodeError:
-            messagebox.showerror('Error','You need to copy it to your clipboard manually.')
-def switchButtonState(button, name):
-    global wrappers
-    if button['relief'] == GROOVE: # turn off
-        button['relief'] = FLAT
-        wrappers[name] = 0
-    else: # turn on
-        button['relief'] = GROOVE
-        wrappers[name] = 1
-def remover(name):
-    global wd
-    switchButtonState(wd[name], name)
-    wrapper()
-    
-def newline_remover(string):
-    new = ''
-    length = len(string)
-    for i,ch in enumerate(string):
-        new += ' ' if ch == '\n' else ch  
-    return new.strip()
-b0 = Button(root,text = "\\n",command = lambda: remover('newline'))
-def brackets_remover(string):
-    new = ''
-    d = {'[': ']', '(': ')', '{': '}', '（': '）',\
-       '【': '】', '<': '>', '《': '》',\
-       '『': '』', '「': '」'}
-    rd = { d[key] : key for key in d } 
-    left = { key : 0 for key in d }
-    right = []
-    for ch in string:
-        if ch in d:
-            left[ch] += 1
-            right.append(d[ch])
-        elif ch in right:
-            left[rd[ch]] -= 1
-            right.remove(ch)
-        elif all(left[b] == 0 for b in left):
-            new += ch
-    return new
-b1 = Button(root,text = '([{}])',command = lambda: remover('brackets'))
-def numbers_remover(string):
-    new = ''
-    for ch in string:
-        new += '' if ch.isnumeric() else ch
-    return new
-b2 = Button(root,text = '1',command = lambda: remover('numbers'))
-def smart_newline_remover(string):
-    new = ''
-    length = len(string)
-    notEnd = [',', '，', ';', ':', \
-              '[', ']','(', ')','{', '}','（', '）',\
-              '【', '】','<', '>','《', '》',\
-              '『', '』','「', '」']
-    noSpace = ['•', '-', '–', '—']
-    for i, ch in enumerate(string):
-        if ch == '\n' and i and i + 1 < length:
-            if string[i + 1] not in noSpace and \
-               (string[i - 1].isalnum() or string[i - 1] in notEnd):
-                new += ' '
-            elif string[i - 1] in noSpace:
-                new += ''
+    def monitor(self, event):
+        t = gettxt(self.t1)
+        if not t or t == self.history[-1]:
+            return
+        if self.ctrlCounter and 'Control' in event.keysym:
+            self.ctrlCounter -= 1
+        elif self.yzCounter and event.keysym in ['y', 'z']:
+            self.yzCounter -= 1
+        else:
+            self.history = self.history[:self.pointer + 1] + [t]
+            self.pointer = len(self.history) - 1
+        self.wrapper()
+        self.countLines()
+        self.countChars()
+    def countLines(self):
+        if self.wrappers['lines']: 
+            count = lambda t: t.count('\n')+(t[-1] != '\n') if t else 0
+            up = count(gettxt(self.t1))
+            down = count(gettxt(self.t2))
+            self.w1['text'] = f'Lines↑{up}↓{down}'
+        else:
+            self.w1['text'] = 'LineCount'
+    def lineCount(self):
+        self.wrappers['lines'] = int(not self.wrappers['lines'])
+        self.countLines()
+    def countChars(self):
+        if self.wrappers['chars']:
+            up = len(gettxt(self.t1))
+            down = len(gettxt(self.t2))
+            self.w2['text'] = f'Chars↑{up}↓{down}'
+        else:
+            self.w2['text'] = 'CharCount'
+    def charCount(self):
+        self.wrappers['chars'] = int(not self.wrappers['chars'])
+        self.countChars()
+    def clear(self):
+        deltxt(self.t1)
+        deltxt(self.t2)
+    def autocopy(self):
+        self.switchButtonState(self.w4, 'autocopy')
+        ez.fwrite(self.btxt, self.wrappers)
+        ez.cpc(gettxt(self.t2))
+    def paste(self):
+        win32clipboard.OpenClipboard()
+        data = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
+        instxt(self.t1, data)
+        self.wrapper()
+    def undo(self):
+        if not self.pointer: return
+        self.pointer -= 1
+        deltxt(self.t1)
+        instxt(self.t1, self.history[self.pointer])
+        self.ctrlCounter = self.yzCounter = 1
+    def ButtonUndo(self):
+        self.undo()
+        self.wrapper()
+    def redo(self):
+        if self.pointer == len(self.history) - 1: return
+        self.pointer += 1
+        deltxt(self.t1)
+        instxt(self.t1, self.history[self.pointer])
+        self.ctrlCounter = self.yzCounter = 1
+    def ButtonRedo(self):
+        self.redo()
+        self.wrapper()
+    def fontSize(self):
+        value = int(self.w8.get())
+        self.wrappers['fontsize'] = value
+        self.w8['label'] = 'FontSize: ' + str(value)
+        self.t2['font'] = ('TkFixedFont', value)
+    def wrapper(self):
+        string = gettxt(self.t1)
+        for func in self.wd:
+            if self.wrappers.get(func, 0):
+                if func == 'newline': string = self.newline_remover(string)
+                elif func == 'brackets': string = self.brackets_remover(string)
+                elif func == 'numbers': string = self.numbers_remover(string)
+                elif func == 'smartnl': string = self.smart_newline_remover(string)
+                self.wd[func]['relief'] = GROOVE
+        
+        if self.wrappers['space']: string = self.space_remover(string)
+        if self.wrappers['translate']: string = self.translate(string)
+        ez.fwrite(self.btxt, self.wrappers)
+        deltxt(self.t2)
+        instxt(self.t2, string)
+        self.countLines()
+        self.countChars()
+        if self.wrappers['autocopy']:
+            try:
+                ez.cpc(string)
+            except UnicodeError:
+                messagebox.showerror('Error','You need to copy it to your clipboard manually.')
+    def switchButtonState(self, button, name):
+        if button['relief'] == GROOVE: # turn off
+            button['relief'] = FLAT
+            self.wrappers[name] = 0
+        else: # turn on
+            button['relief'] = GROOVE
+            self.wrappers[name] = 1
+    def remover(self, name):
+        self.switchButtonState(self.wd[name], name)
+        self.wrapper()
+    def newline_remover(self, string):
+        new = ''
+        for i,ch in enumerate(string):
+            new += ' ' if ch == '\n' else ch  
+        return new.strip()
+    def brackets_remover(self, string):
+        new = ''
+        left = { key: 0 for key in self.brackets }
+        right = []
+        for ch in string:
+            if ch in self.brackets:
+                left[ch] += 1
+                right.append(self.brackets[ch])
+            elif ch in right:
+                left[self.rbrackets[ch]] -= 1
+                right.remove(ch)
+            elif all(left[b] == 0 for b in left):
+                new += ch
+        return new
+    def numbers_remover(self, string):
+        new = ''
+        for ch in string:
+            new += '' if ch.isnumeric() else ch
+        return new
+    def smart_newline_remover(self, string):
+        new = ''
+        length = len(string)
+        notEnd = [',', '，', ';', ':', \
+                '[', ']','(', ')','{', '}','（', '）',\
+                '【', '】','<', '>','《', '》',\
+                '『', '』','「', '」']
+        noSpace = ['•', '-', '–', '—']
+        for i, ch in enumerate(string):
+            if ch == '\n' and i and i + 1 < length:
+                if string[i + 1] not in noSpace and \
+                (string[i - 1].isalnum() or string[i - 1] in notEnd):
+                    new += ' '
+                elif string[i - 1] in noSpace:
+                    new += ''
+                else:
+                    new += ch
             else:
                 new += ch
-        else:
-            new += ch
-    return new.strip()
-b3 = Button(root,text = 'smartnl',command = lambda: remover('smartnl'))
-def space_remover(string):
-    return ez.sub(string, ' ,',',','  ',' ',' .','.',' 。','。',' ，','，')
-b4 = Button(root,text = 'space',command = lambda: remover('space'))
-def translate(string):
-    try:
-        if len(string) > 5000:
-            messagebox.showwarning('Warning', 'String length can\'t exceeds 5000!')
-        return ez.translate(string)
-    except Exception as e:
-        if e is UnicodeError:
-            messagebox.showerror('Error', 'Can\'t translate!')
-        else:
-            messagebox.showerror('Error', 'No Network!')
-        remover('translate')
-        return string
-b5 = Button(root,text = 'translate',command = lambda: remover('translate'))
+        return new.strip()
+    def space_remover(self, string):
+        return ez.sub(string, ' ,',',','  ',' ',' .','.',' 。','。',' ，','，')
+    def translate(self, string):
+        try:
+            if len(string) > 5000:
+                messagebox.showwarning('Warning', 'String length can\'t exceeds 5000!')
+            return ez.translate(string)
+        except Exception as e:
+            if e is UnicodeError:
+                messagebox.showerror('Error', 'Can\'t translate!')
+            else:
+                messagebox.showerror('Error', 'No Network!')
+            self.remover('translate')
+            return string
+        
 
-buttons = [b0, b1, b2, b3, b4, b5]
-widgets = [w0, w1, w2, w3, w4, w5, w6, w7]
-wd = {'newline': b0, 'brackets': b1, 'numbers': b2, 'smartnl': b3, 'autocopy': w4, 'space': b4, 'translate': b5}
-columnspanLength = max(len(widgets),len(buttons))
-for i, w in enumerate(buttons):
-    w.configure(relief = FLAT,bg = 'SeaGreen1')
-    w.grid(row = 0,column = i,sticky = NS)
-t1.grid(row = 1,column = 0,columnspan = columnspanLength, sticky = EW)
-for i, w in enumerate(widgets):
-    w.configure(relief = FLAT,bg = 'thistle1')
-    w.grid(row = 2,column = i,sticky = NS)
-t2 = Text(root)
-t2.grid(row = 3,column = 0,columnspan = columnspanLength, sticky = EW)
-for col in range(columnspanLength): root.columnconfigure(col, weight = 1)
-for row in range(4): root.rowconfigure(row, weight = 1)
-
-wrapper()
-root.mainloop()
-
+app = Simplifier()
+app.mainloop()
 ez.py2pyw('simplifier.py')
